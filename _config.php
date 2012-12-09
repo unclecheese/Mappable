81f7e2fb85e678b63da41cda38f734c0d0e31203
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-12-09 23:12:33 +0700

    UPGRADE: Added path to the config file

--- _config.php
+++ _config.php
@@ -3,3 +3,9 @@
 Object::add_extension("DataObject","MappableData");
 
 Object::add_extension("DataObjectSet","MappableDataObjectSet");
+//define global path to Components' root folder
+if(!defined('MAPPABLE_MODULE_PATH'))
+{
+	define('MAPPABLE_MODULE_PATH', rtrim(basename(dirname(__FILE__))));
+}
+?>
