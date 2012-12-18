5e699c161b7e0534628b8d2f4194ecc38beb81b9
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-12-18 11:41:21 +0700

    WIP: Change reference from google map to be generic

--- code/MappableDataObjectSet.php
+++ code/MappableDataObjectSet.php
@@ -8,10 +8,10 @@
  */
 class MappableDataObjectSet extends Extension {
 
-	public function GoogleMap($width = null, $height = null) {
-		$gmap = GoogleMapUtil::get_map($this->owner);
-		$w = $width ? $width : GoogleMapUtil::$map_width;
-		$h = $height ? $height : GoogleMapUtil::$map_height;
+	public function RenderMap($width = null, $height = null) {
+		$gmap = MapUtil::get_map($this->owner);
+		$w = $width ? $width : MapUtil::$map_width;
+		$h = $height ? $height : MapUtil::$map_height;
 		$gmap->setSize($w,$h);
 		return $gmap;
 	}
