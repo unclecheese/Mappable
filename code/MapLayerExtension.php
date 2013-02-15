a20b317184be3805a8388c708360834ff5207238
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-02-15 20:28:20 +0700

    ENHANCEMENT: Extension to add map layers to an object

---
+++ code/MapLayerExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+class MapLayerExtension extends DataExtension {
+
+  static $many_many = array(
+      'MapLayers' => 'MapLayer'
+   );
+
+
+  static $belongs_many_many_extraFields = array(
+    'MapLayers' => array(
+      'SortOrder' => "Int"
+    )
+  );
+
+
+  public function updateCMSFields( FieldList $fields ) {
+
+        
+       $gridConfig2 = GridFieldConfig_RelationEditor::create();
+    $gridConfig2->getComponentByType( 'GridFieldAddExistingAutocompleter' )->setSearchFields( array( 'Title' ) );
+    $gridConfig2->getComponentByType( 'GridFieldPaginator' )->setItemsPerPage( 100 );
+
+    $gridField2 = new GridField( "Map Layers", "Map Layers:", $this->owner->MapLayers(), $gridConfig2 );
+    $fields->addFieldToTab( "Root.MapLayers", $gridField2 );
+
+  }
+
+
+ 
+
+}
+
+?>
