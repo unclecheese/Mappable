eaf44ef401d8fe406da9056d22b841dd99850796
Werner Krauss <werner.krauss@netwerkstatt.at>
2013-09-08 14:15:39 +0000

    moved most config to yml

--- _config.php
+++ _config.php
@@ -1,15 +1,7 @@
 <?php
 
-Object::add_extension("DataObject","MappableData");
-Object::add_extension("DataList","MappableDataObjectSet");
 //define global path to Components' root folder
 if(!defined('MAPPABLE_MODULE_PATH'))
 {
 	define('MAPPABLE_MODULE_PATH', rtrim(basename(dirname(__FILE__))));
 }
-
-// allow geographical format files to be uploaded
-File::$allowed_extensions[] = 'gpx' ;
-File::$allowed_extensions[] = 'kml' ;
-
-?>
