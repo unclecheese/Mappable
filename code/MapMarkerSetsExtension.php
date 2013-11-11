fea63a2570de4ad74d162499c8bd05271f2d0757
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-11-11 22:44:56 +0700

    WIP: Map marker sets, work in progress

---
+++ code/MapMarkerSetsExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+class MapMarkerSetsExtension extends DataExtension {
+
+  static $many_many = array(
+      'MapMarkerSets' => 'MapMarkerSet'
+   );
+
+
+  static $belongs_many_many_extraFields = array(
+      'MapMarkerSets' => array(
+          'SortOrder' => "Int"
+        )
+  );
+
+
+  public function updateCMSFields( FieldList $fields ) {
+
+        
+    $gridConfig2 = GridFieldConfig_RelationEditor::create();
+    $gridConfig2->getComponentByType( 'GridFieldAddExistingAutocompleter' )->setSearchFields( array( 'Title' ) );
+    $gridConfig2->getComponentByType( 'GridFieldPaginator' )->setItemsPerPage( 100 );
+
+    $gridField2 = new GridField( "MapMarkerSets", "MapMarkers", $this->owner->MapMarkerSets(), $gridConfig2 );
+    $fields->addFieldToTab( "Root.MapMarkerSets", $gridField2 );
+
+  }
+
+}
