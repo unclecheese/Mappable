58a22034f2591598fdfbef7a27bb4e9ff02ba595
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-04 21:34:58 +0700

    ENHANCEMENT: Addition of short code parser for google streetview

--- _config.php
+++ _config.php
@@ -5,3 +5,6 @@ if(!defined('MAPPABLE_MODULE_PATH'))
 {
 	define('MAPPABLE_MODULE_PATH', rtrim(basename(dirname(__FILE__))));
 }
+
+ShortcodeParser::get('default')->register('GoogleStreetView',array('GoogleStreetViewShortCodeHandler','parse_googlestreetview'));
+
