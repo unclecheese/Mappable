8090c5e76dcab55baf28b1c0ec190d16f540efb6
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-07 21:13:16 +0700

    MINOR: Whitespace tidyup

--- code/MapLayerExtension.php
+++ code/MapLayerExtension.php
@@ -6,7 +6,6 @@ class MapLayerExtension extends DataExtension {
       'MapLayers' => 'MapLayer'
    );
 
-
   static $belongs_many_many_extraFields = array(
     'MapLayers' => array(
       'SortOrder' => "Int"
@@ -15,20 +14,10 @@ class MapLayerExtension extends DataExtension {
 
 
   public function updateCMSFields( FieldList $fields ) {
-
-        
-       $gridConfig2 = GridFieldConfig_RelationEditor::create();
+    $gridConfig2 = GridFieldConfig_RelationEditor::create();
     $gridConfig2->getComponentByType( 'GridFieldAddExistingAutocompleter' )->setSearchFields( array( 'Title' ) );
     $gridConfig2->getComponentByType( 'GridFieldPaginator' )->setItemsPerPage( 100 );
-
     $gridField2 = new GridField( "Map Layers", "Map Layers:", $this->owner->MapLayers(), $gridConfig2 );
     $fields->addFieldToTab( "Root.MapLayers", $gridField2 );
-
   }
-
-
- 
-
 }
-
-?>
