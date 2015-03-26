2801ecb1dc97681e75afe398eb3302a4f1e27868
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-27 01:28:04 +0700

    FIX: Instead of calling function to render a map, call one that primes values to be rendered by a callback

--- javascript/google/map.google.template.js
+++ javascript/google/map.google.template.js
@@ -1,15 +1,9 @@
-function initialize() {
-	var mapOptions = {
-		center: { lat: $Latitude, lng: $Longitude},
-		zoom: $Zoom,
-		mapTypeId: google.maps.MapTypeId.$MapType
-	};
-	var map = new google.maps.Map(document.getElementById('$DomID'),
-		mapOptions);
-	if ($AllowFullScreen == 1) {
-		map.controls[google.maps.ControlPosition.TOP_RIGHT].push(
-			FullScreenControl(map, "Full Screen", "Original Size")
-		);
-	}
+var options = {
+	latitude: $Latitude,
+	longitude: $Longitude,
+	zoom: $Zoom,
+	maptype: '$MapType',
+	domid: '$DomID',
+	allowfullscreen: $AllowFullScreen
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+registerShortcodeMap(options);
