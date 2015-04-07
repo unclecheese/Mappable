b88375c04a9f2be3e7e6016dc0bcbc18aacc2524
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-07 14:28:17 +0700

    ENHANCEMENT: Addition of map icon for reports menu

--- code/PointsOfInterestAdmin.php
+++ code/PointsOfInterestAdmin.php
@@ -4,6 +4,7 @@ class PointsOfInterestAdmin extends ModelAdmin {
 	private static $managed_models = array('PointsOfInterestLayer','PointOfInterest');
 	private static $url_segment = 'poi';
 	private static $menu_title = 'Points of Interest';
+	private static $menu_icon = '/mappable/icons/menuicon.png';
 
 	static $has_one = array(
 	    'DefaultIcon' => 'Image'
