c0040ab4cc860e1307d82687e784efd7812961ed
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-09 15:49:17 +0700

    ENHANCEMENT: One can now pass parameters to a Mappable or MappableDataObjectSet to be rendered in the map information windows

--- code/MappableData.php
+++ code/MappableData.php
@@ -8,8 +8,24 @@
  */
 class MappableData extends Extension {
 
+	/**
+	 * Optional template values for the map info windows
+	 */
+	private $MarkerTemplateValues = null;
+
+	/**
+	 * Pass through values to the markers so that when rendering the map info windows, these
+	 * parameters are available to the template.  This is of course optional
+	 *
+	 * @param array $values hash array of template key to template value
+	 */
+	public function setMarkerTemplateValues($values) {
+		$this->MarkerTemplateValues = $values;
+	}
+
+
 	public function getRenderableMap($width = null, $height = null, $zoom = 9) {
-		$gmap = MapUtil::get_map(new ArrayList(array($this->owner)));
+		$gmap = MapUtil::get_map(new ArrayList(array($this->owner)), $this->MarkerTemplateValues);
 		$w = $width ? $width : MapUtil::$map_width;
 		$h = $height ? $height : MapUtil::$map_height;
 		$gmap->setSize($w,$h);
