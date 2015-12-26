d64a3c7f869142b9e98d9c78d202507bf0adc5b5
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-26 17:10:11 +0700

    MINOR: Reset map counter method for testing purposes

--- code/shortcodes/GoogleMapShortCodeHandler.php
+++ code/shortcodes/GoogleMapShortCodeHandler.php
@@ -64,4 +64,11 @@ class GoogleMapShortCodeHandler {
 		//return the template customised with the parmameters
 		return $template->process(new ArrayData($customised));
 	}
+
+	/**
+	 * This is only used for testing, otherwise the sequence of tests change the number returned
+	 */
+	public static function resetCounter() {
+		self::$gsv_ctr = 1;
+	}
 }
