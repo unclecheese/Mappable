6bd0586d48561d351f67d7c24b7d84573bcd3ed6
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-26 17:05:35 +0700

    MINOR: Horizontal white, debug removal

--- code/MapField.php
+++ code/MapField.php
@@ -10,40 +10,35 @@ class MapField extends DatalessField {
 		protected $headingLevel = 2;
 		private $divId;
 
+		/**
+		 * @param string $name
+		 * @param string $title
+		 */
 		function __construct($name, $title = null, $headingLevel = 2, $allowHTML = false, $form = null) {
 			$this->divId = $name;
-				// legacy handling for old parameters: $title, $heading, ...
-				// instead of new handling: $name, $title, $heading, ...
-				$args = func_get_args();
-				if(!isset($args[1]) || is_numeric($args[1])) {
-						$title = (isset($args[0])) ? $args[0] : null;
-						// Use "HeaderField(title)" as the default field name for a HeaderField;
-						// if it's just set to title then we risk causing accidental duplicate-field creation.
+			// legacy handling for old parameters: $title, $heading, ...
+			// instead of new handling: $name, $title, $heading, ...
+			$args = func_get_args();
+			if(!isset($args[1]) || is_numeric($args[1])) {
+					$title = (isset($args[0])) ? $args[0] : null;
+					// Use "HeaderField(title)" as the default field name for a HeaderField;
+					// if it's just set to title then we risk causing accidental duplicate-field creation.
 
-						// this means i18nized fields won't be easily accessible through fieldByName()
-						$name = 'MapField' . $title;
-						$headingLevel = (isset($args[1])) ? $args[1] : null;
-						$allowHTML = (isset($args[2])) ? $args[2] : null;
-						$form = (isset($args[3])) ? $args[3] : null;
-				}
+					// this means i18nized fields won't be easily accessible through fieldByName()
+					$name = 'MapField' . $title;
+					$headingLevel = (isset($args[1])) ? $args[1] : null;
+					$allowHTML = (isset($args[2])) ? $args[2] : null;
+					$form = (isset($args[3])) ? $args[3] : null;
+			}
 
-				if($headingLevel) $this->headingLevel = $headingLevel;
-				$this->allowHTML = $allowHTML;
-				parent::__construct($name, $title, null, $allowHTML, $form);
+			if($headingLevel) $this->headingLevel = $headingLevel;
+			$this->allowHTML = $allowHTML;
+			parent::__construct($name, $title, null, $allowHTML, $form);
 		}
 
 		function Field($properties = array()) {
 				Requirements::javascript('framework/thirdparty/jquery/jquery.js');
 				Requirements::javascript('framework/thirdparty/jquery-livequery/jquery.livequery.js');
-			 // Requirements::javascript('http://maps.google.com/maps/api/js?sensor=false');
-
-
-			 // Requirements::javascript('mappable/javascript/mapField.js');
-
-
-
-
-
 				$attributes = array(
 						'class' => 'middleColumn',
 						'id' => $this->divId,
