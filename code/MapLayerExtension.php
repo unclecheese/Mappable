44afa59950c778666ed42cad9ac97044897e1627
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-05 17:11:30 +0700

    FIX: Move testing for map layers to the MapLayerExtension using the updateBasicMap extension point

--- code/MapLayerExtension.php
+++ code/MapLayerExtension.php
@@ -27,4 +27,30 @@ class MapLayerExtension extends DataExtension
         );
         $fields->addFieldToTab('Root.MapLayers', $gridField2);
     }
+
+    /**
+     * Only set has geo to true if layers exist
+     * @param  boolean &$hasGeo will be set to true if any layers
+     */
+    public function updateHasGeo(&$hasGeo)
+    {
+        if ($this->owner->MapLayers()->count() > 0) {
+            $hasGeo = true;
+        }
+    }
+
+    /**
+     * Add layers if the exist to a map from the MapExtension
+     * @param  MapAPI &$map      object representing the map
+     * @param  boolean &$autozoom true to auto zoom, false not to
+     */
+    public function updateBasicMap(&$map, &$autozoom)
+    {
+        // add any KML map layers
+        foreach ($this->owner->MapLayers() as $layer) {
+            $map->addKML($layer->KmlFile()->getAbsoluteURL());
+            // we have a layer, so turn on autozoom
+            $autozoom = true;
+        }
+    }
 }
